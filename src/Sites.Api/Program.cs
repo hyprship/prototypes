5f1467b6abcbc1bfbb8d9d56f9e73a576a0bc5e7
hyprxdev <180069990+hyprxdev@users.noreply.github.com>
2025-05-24 16:26:42 -0500

    format program.cs

--- src/Sites.Api/Program.cs
+++ src/Sites.Api/Program.cs
@@ -21,19 +21,25 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
+#pragma warning disable SCS0005 // Weak random number generator.
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast(
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]))
         .ToArray();
+#pragma warning restore SCS0005 // Weak random number generator.
     return forecast;
 })
 .WithName("GetWeatherForecast");
 
-app.Run();
+await app.RunAsync();
 
+#pragma warning disable S3903 // Types should be defined in named namespaces
+#pragma warning disable SA1649 // File name should match first type name
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+#pragma warning restore SA1649 // File name should match first type name
+#pragma warning restore S3903 // Types should be defined in named namespaces
 {
     public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
 }
